3764b83cdab428d513265bd78863926128645167
josharms00 <josharms00@gmail.com>
2020-08-11 13:31:42 -0400

    Created function for preprocessing of data.

--- movie_sentiment.py
+++ movie_sentiment.py
@@ -12,8 +12,7 @@ from sklearn.svm import LinearSVC
 from sklearn.linear_model import 
 from statistics import mode
 
-# create lammentizer
-lemmatizer = WordNetLemmatizer()
+
 
 class AlgoVote(ClassifierI):
     def __init__(self, *classifiers):
@@ -28,7 +27,39 @@ class AlgoVote(ClassifierI):
 
         return mode(votes)
 
-# import training data
-# 5300 of each review
-pos_rev = open("movie review data/positive.txt", "r").read()
-neg_rev = open("movie review data/negative.txt", "r").read()
+
+def preprocessing():
+    # create lammentizer
+    lemmatizer = WordNetLemmatizer()
+
+    # create set of useless words
+    stop_words = set(stopwords.words("english")) 
+
+    # import training data
+    # 5300 of each review
+    pos_rev = open("movie review data/positive.txt", "r").read()
+    neg_rev = open("movie review data/negative.txt", "r").read()
+
+    documents = []
+
+    for r in pos_rev.split("\n"):
+        documents.append( (r, "pos") )
+
+    for r in neg_rev.split("\n"):
+        documents.append( (r, "neg") )
+
+    all_words = []
+
+    for d in documents:
+        _words = word_tokenize(d[0])
+        for w in _words:
+            if w not in stop_words:
+                all_words.append(lemmatizer.lemmatize(w.lower()))
+
+    # convert to frequncy distribution of words
+    all_words = nltk.FreqDist(all_words)
+
+
+
+if __name__ == "__main__":
+    
