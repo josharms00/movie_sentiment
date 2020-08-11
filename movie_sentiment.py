901c41cdd28d2f85bccd9207a0acf4999baa44ee
josharms00 <josharms00@gmail.com>
2020-08-11 13:24:26 -0400

    Added class for algorithm voting system.

--- movie_sentiment.py
+++ movie_sentiment.py
@@ -10,6 +10,25 @@ from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import 
+from statistics import mode
 
 # create lammentizer
 lemmatizer = WordNetLemmatizer()
+
+class AlgoVote(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        return mode(votes)
+
+# import training data
+# 5300 of each review
+pos_rev = open("movie review data/positive.txt", "r").read()
+neg_rev = open("movie review data/negative.txt", "r").read()
