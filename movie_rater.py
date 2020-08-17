bfeed7d183aae0d75c88ed2f542c5f9119aa959f
josharms00 <josharms00@gmail.com>
2020-08-17 11:29:19 -0400

    Added code to connect to twitter api.

--- movie_rater.py
+++ movie_rater.py
@@ -2,9 +2,12 @@ import sentiment as s
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import json
+import argparse
 
 # IMPORTANT: must have twitter dev account and access tokens to use this
 def getKeys():
+    # replace with own path to keys
     f_keys = open("C:/Users/josh/Projects/tokens.txt", "r").read()
 
     keys = f_keys.split("\n")
@@ -12,6 +15,39 @@ def getKeys():
     return keys[:4]
 
 ckey, csecret, atoken, asecret = getKeys()
+
+class tweet_listener(StreamListener):
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+
+        tweet = all_data["text"]
+
+        print(tweet)
+
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-m', '--movie',
+                action="store", dest="movie",
+                help="pick a movie")
+
+    args = parser.parse_args()
+
+    auth = OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
+
+    if args.movie:
+        twitterStream = Stream(auth, tweet_listener())
+        twitterStream.filter(track=[args.movie])
+    else:
+        print("You need to request a movie to rate.")
 
 
 # print(s.sentiment("I’m like an hour fifteen into ONCE UPON A TIME... IN HOLLYWOOD and it’s beautiful and charming and absolutely nothing has happened yet"))
