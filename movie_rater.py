8c7ddee6f2e7c52005f46de63e9189b693e54b04
josharms00 <josharms00@gmail.com>
2020-08-17 09:44:35 -0400

    Added function to parse file containing twitter access tokens.

--- movie_rater.py
+++ movie_rater.py
@@ -1,3 +1,19 @@
 import sentiment as s
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 
-print(s.sentiment("This movie was awesome! The acting was great!"))
+# IMPORTANT: must have twitter dev account and access tokens to use this
+def getKeys():
+    f_keys = open("C:/Users/josh/Projects/tokens.txt", "r").read()
+
+    keys = f_keys.split("\n")
+
+    return keys[:4]
+
+ckey, csecret, atoken, asecret = getKeys()
+
+
+# print(s.sentiment("I’m like an hour fifteen into ONCE UPON A TIME... IN HOLLYWOOD and it’s beautiful and charming and absolutely nothing has happened yet"))
+
+# print(s.sentiment("The movie is what, 3 hours? And the first 2 hours 45 min was a bit ramble-y but promised either an exciting convergence of the A and B plots, or an emotional reckoning of an old star being given a second chance. We got neither and in the last 15 minutes we got excessive gore as two grown men fight and kill teenagers. Which. I should have seen coming I GUESS."))
